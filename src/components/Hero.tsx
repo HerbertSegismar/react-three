c256e0f0d828bdbdaf05c4ac014589343597159d
Herb Segismar <herbert.segismar@yahoo.com>
2025-07-03 23:36:43 +1200

    3D computer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,25 @@
+import ComputersCanvas from "./canvas/ComputersCanvas";
 
 const Hero = () => {
   return (
-    <div> Hero</div>
-  )
+    <section className="relative w-full h-screen mx-auto">
+      <div className="paddingX absolute inset-0 top-[120px] max-w-7xl mx-auto flex items-start gap-5">
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="size-5 rounded-full bg-themePurple" />
+          <div className="sm:h-80 h-40 w-1 bg-gradient-to-b from-[#804dee] to-[#3c3350]/0" />
+        </div>
+        <div>
+          <h1 className="text-white heroHeadText">
+            Hi, I&apos;m <span className="text-themePurple">Herb</span>
+          </h1>
+          <p className="heroSubText mt-2 text-white-100">
+            I develop 3D visuals, user <br className="sm:block hidden"/> interfaces, and web applications.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
 }
 
 export default Hero
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,8 @@ import { navLinks } from '../constants';
 const Navbar = () => {
 
   const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="paddingX w-full flex items-center py-5 fixed top-0 z-20 bg-primary">
       <div className="w-full h-24 flex justify-between items-center max-w-7xl mx-auto">
